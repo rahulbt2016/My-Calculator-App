a816efb90a855a0dd71da597b56a5a87bf274c8b
Rahul <rahulbt2016@gmail.com>
2022-10-23 17:03:24 -0400

    Added addition functionality

--- app/src/main/java/com/nseaf/mycalculator/Calculator.java
+++ app/src/main/java/com/nseaf/mycalculator/Calculator.java
@@ -8,16 +8,19 @@ public class Calculator {
     double realNumber;
     double realNumber2;
     boolean isIntNumber;
+    boolean isIntNumber2;
     boolean numHasRadixPoint;
     long memoryInt;
     double memoryDouble;
     boolean isIntMemory;
     String operation;
+    boolean isFirstOperation;
 
     public Calculator() {
         this.numberString = "0";
         this.detailsString = "";
         this.isIntNumber = true;
+        this.isIntNumber2 = true;
         this.isIntMemory = true;
         this.numHasRadixPoint = false;
         this.intNumber = 0;
@@ -59,6 +62,7 @@ public class Calculator {
         realNumber2 = 0.0;
         operation = "";
         isIntNumber=true;
+        isIntNumber2=true;
         numHasRadixPoint=false;
 
     }
@@ -110,24 +114,82 @@ public class Calculator {
     }
 
     public void divClicked() {
+        operationClicked();
+        operation = "/";
+        detailsString = "Operation: ÷";
     }
 
     public void mulClicked() {
+        operationClicked();
+        operation = "*";
+        detailsString = "Operation: ×";
     }
 
     public void minClicked() {
+        operationClicked();
+        operation = "-";
+        detailsString = "Operation: -";
     }
 
     public void plusClicked() {
+        operationClicked();
+        operation = "+";
+        detailsString = "Operation: +";
     }
 
     public void equalClicked() {
-        if (operation != "") {
+        if (!operation.equalsIgnoreCase("")) {
 
-            if (operation == "+") {
+            if (operation.equalsIgnoreCase("+")) {
+                if (isIntNumber && isIntNumber2) {
+                    intNumber += intNumber2;
+                    isIntNumber = true;
+                }
+                else if (!isIntNumber && !isIntNumber2){
+                    realNumber += realNumber2;
+                    isIntNumber = false;
+                }
+                else if (!isIntNumber && isIntNumber2) {
+                    realNumber += intNumber2;
+                    isIntNumber = false;
+                }
+                else if (isIntNumber && !isIntNumber2) {
+                    realNumber = intNumber + realNumber2;
+                    isIntNumber = false;
+                }
+
+                if (!isIntNumber) {
+                    if (realNumber % 1 == 0.0) {
+                        intNumber = (int) realNumber;
+                        isIntNumber = true;
+
+                    }
+                }
+
+                numberString = isIntNumber ?  String.valueOf(intNumber) : String.valueOf(realNumber);
+            }
+
+            else if (operation.equalsIgnoreCase("-")) {
+
+            }
+
+            else if (operation.equalsIgnoreCase("*")) {
+
+            }
+
+            else if (operation.equalsIgnoreCase("/")) {
 
             }
         }
+
+        //Erasing operand 2, once the operation is done
+        isIntNumber2 = true;
+        intNumber2 = 0;
+        realNumber2 = 0.0;
+
+        detailsString = "Answer";
+        operation = "";
+        isFirstOperation = true;
     }
 
     public void radicalPointClicked() {
@@ -139,5 +201,33 @@ public class Calculator {
             realNumber = (float) intNumber;
             intNumber = 0;
         }
+    }
+
+    //For any operation, shifting the value of the operand 1 to operand 2,
+    //we can record new number as operand 1
+    private void operationClicked() {
+        if (isFirstOperation) {
+            isFirstOperation = false;
+            if(isIntNumber) {
+                intNumber2 = intNumber;
+                isIntNumber2 = true;
+                intNumber = 0;
+            }
+            else {
+                realNumber2 = realNumber;
+                isIntNumber2 = false;
+                intNumber = 0;
+                isIntNumber = true;
+            }
+
+        }
+
+        else {
+            equalClicked();
+            operationClicked();
+        }
+
+        numHasRadixPoint = false;
+        numberString = "0";
     }
 }
